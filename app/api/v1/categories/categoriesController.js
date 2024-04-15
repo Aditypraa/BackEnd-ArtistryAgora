6517d31dcac496187af060a40fcaf86b5e254072
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-04-16 01:08:45 +0700

    feat(custom Error) : Memperbaiki custom error dan memisahkannya di folder service/mongoose

--- app/api/v1/categories/categoriesController.js
+++ app/api/v1/categories/categoriesController.js
@@ -1,28 +1,17 @@
-const CategoriesModel = require("./categoriesModel");
+const { StatusCodes } = require("http-status-codes");
+
+const {
+  getAllCategories,
+  createCategories,
+  getOneCategories,
+  updateCategories,
+  deleteCategories,
+} = require("../../../services/mongoose/categoriesMongoose");
 
 const index = async (req, res, next) => {
   try {
-    const result = await CategoriesModel.find().select("_id name");
-    res.status(200).json({
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-
-const find = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await CategoriesModel.findOne({ _id: id });
-
-    if (!result) {
-      return res.status(404).json({
-        message: "Id Categories Tidak Ditemukan",
-      });
-    }
-
-    res.status(200).json({
+    const result = await getAllCategories();
+    res.status(StatusCodes.OK).json({
       data: result,
     });
   } catch (err) {
@@ -32,9 +21,20 @@ const find = async (req, res, next) => {
 
 const create = async (req, res, next) => {
   try {
-    const { name } = req.body;
-    const result = await CategoriesModel.create({ name });
-    res.status(201).json({
+    const result = await createCategories(req);
+    res.status(StatusCodes.CREATED).json({
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const find = async (req, res, next) => {
+  try {
+    const result = await getOneCategories(req);
+
+    res.status(StatusCodes.OK).json({
       data: result,
     });
   } catch (err) {
@@ -44,21 +44,9 @@ const create = async (req, res, next) => {
 
 const update = async (req, res, next) => {
   try {
-    const { id } = req.params;
-    const { name } = req.body;
-    const result = await CategoriesModel.findOneAndUpdate(
-      { _id: id },
-      { name },
-      { new: true, runValidators: true }
-    );
+    const result = await updateCategories(req);
 
-    if (!result) {
-      return res.status(404).json({
-        message: "Id Categories Tidak Ditemukan",
-      });
-    }
-
-    res.status(200).json({
+    res.status(StatusCodes.OK).json({
       data: result,
     });
   } catch (err) {
@@ -68,9 +56,8 @@ const update = async (req, res, next) => {
 
 const destroy = async (req, res, next) => {
   try {
-    const { id } = req.params;
-    const result = await CategoriesModel.findByIdAndDelete(id);
-    res.status(200).json({
+    const result = await deleteCategories(req);
+    res.status(StatusCodes.OK).json({
       data: result,
     });
   } catch (err) {
---
+++ app/services/mongoose/categoriesMongoose.js
@@ -0,0 +1,78 @@
+const CategoriesModel = require("../../api/v1/categories/categoriesModel");
+const { BadRequestError, NotFoundError } = require("../../errors");
+
+const getAllCategories = async () => {
+  const result = await CategoriesModel.find();
+
+  return result;
+};
+
+const createCategories = async (req) => {
+  const { name } = req.body;
+
+  // cari categories dengan field name
+  const check = await CategoriesModel.findOne({ name });
+
+  // apa bila check true / data categories sudah ada maka kita tampilkan error bad request dengan message kategori nama duplikat
+  if (check) throw new BadRequestError("kategori nama duplikat");
+
+  const result = await CategoriesModel.create({ name });
+
+  return result;
+};
+
+const getOneCategories = async (req) => {
+  const { id } = req.params;
+  const result = await CategoriesModel.findOne({ _id: id });
+
+  if (!result) throw new NotFoundError(`Tidak Ada Kategori dengan ID : ${id}`);
+
+  return result;
+};
+
+const updateCategories = async (req) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  // cari categories model dengan field name dan id selain dari yang dikirim dari params
+  const check = await CategoriesModel.findOne({
+    name,
+    _id: { $ne: id },
+  });
+
+  // apa bila check true / data categories sudah ada maka kita tampilkan error bad request dengan message kategori nama duplikat
+  if (check) throw new BadRequestError("kategori nama duplikat");
+
+  const result = await CategoriesModel.findOneAndUpdate(
+    { _id: id },
+    { name },
+    { new: true, runValidators: true }
+  );
+
+  // jika id result false / null maka akan menampilkan error `Tidak ada Kategori dengan id` yang dikirim client
+  if (!result) throw new NotFoundError(`Tidak ada Kategori dengan id :  ${id}`);
+
+  return result;
+};
+
+const deleteCategories = async (req) => {
+  const { id } = req.params;
+
+  const result = await CategoriesModel.findOne({
+    _id: id,
+  });
+
+  if (!result) throw new NotFoundError(`Tidak ada Kategori dengan id :  ${id}`);
+
+  await result.deleteOne({ _id: id });
+
+  return result;
+};
+
+module.exports = {
+  getAllCategories,
+  createCategories,
+  getOneCategories,
+  updateCategories,
+  deleteCategories,
+};
