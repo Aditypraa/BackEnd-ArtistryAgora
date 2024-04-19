885a979affe5012955752f340383c78fcf411857
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-04-19 22:59:36 +0700

    feat(talents) : Menambahkan Middleware pada route Talents

--- app/api/v1/talents/talentsRouter.js
+++ app/api/v1/talents/talentsRouter.js
@@ -2,10 +2,31 @@ const router = require("express").Router();
 
 const { index, find, update, create, destroy } = require("./talentsController");
 
-router.get("/talents", index);
-router.get("/talents/:id", find);
-router.post("/talents", create);
-router.put("/talents/:id", update);
-router.delete("/talents/:id", destroy);
+// Middleware Auth
+const {
+  authenticatedUser,
+  authorizeRoles,
+} = require("../../../middlewares/authMiddlewares");
+
+router.get("/talents", authenticatedUser, authorizeRoles("organizer"), index);
+router.get(
+  "/talents/:id",
+  authenticatedUser,
+  authorizeRoles("organizer"),
+  find
+);
+router.post("/talents", authenticatedUser, authorizeRoles("organizer"), create);
+router.put(
+  "/talents/:id",
+  authenticatedUser,
+  authorizeRoles("organizer"),
+  update
+);
+router.delete(
+  "/talents/:id",
+  authenticatedUser,
+  authorizeRoles("organizer"),
+  destroy
+);
 
 module.exports = router;
