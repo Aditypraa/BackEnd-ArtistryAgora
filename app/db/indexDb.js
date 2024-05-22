2ebee56fbc60acb0bfb964cf7e8fcd26185aca2f
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-05-22 09:29:07 +0700

    (refactor) : add documentasion indexDB.js

--- app/db/indexDb.js
+++ app/db/indexDb.js
@@ -1,8 +1,14 @@
+// 1. Import mongoose
 const mongoose = require("mongoose");
+
+// 2. Import Konfigurasi terkait mongoDB dari app/config/mongoConfig.js
 const { urlDb } = require("../config/mongoConfig");
 
+// 3. Koneksikan ke mongoDB menggunakan konfigurasi yang telah diimport
 mongoose.connect(urlDb);
 
+// 4. Simpan koneksi ke variabel db
 const db = mongoose.connection;
 
+// 5. Export db supaya bisa digunakan di file yang lain yang membutuhkan koneksi ke mongoDB
 module.exports = db;
