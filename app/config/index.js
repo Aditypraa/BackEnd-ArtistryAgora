2fd5ec68fcdb64ad8aabdf76664678975ecbfd4a
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-06-25 04:17:47 +0700

    refactor: Update static file path in app.js and config/index.js

--- app/config/index.js
+++ app/config/index.js
@@ -2,8 +2,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  urlDb: process.env.URL_MONGODB_DEV,
-  // urlDb: process.env.URL_MONGODB_PROD,
+  // urlDb: process.env.URL_MONGODB_DEV,
+  urlDb: process.env.URL_MONGODB_PROD,
   jwtExpiration: process.env.JWT_EXPIRATION,
   jwtSecret: process.env.JWT_SECRET_KEY,
   jwtRefreshTokenExpiration: process.env.JWT_EXPIRATION_REFRESH_TOKEN,
